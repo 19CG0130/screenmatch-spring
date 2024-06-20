efe01e22393cf7bf6a879764fe64e42d75377d8b
19CG0130 <19cg0130@gmail.com>
2024-06-19 18:40:30 -0600

    creating the interactive menu

--- screenmatch/src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
+++ screenmatch/src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
@@ -3,6 +3,7 @@ package com.aluracursos.screenmatch;
 import com.aluracursos.screenmatch.model.DatosEpisodio;
 import com.aluracursos.screenmatch.model.DatosSerie;
 import com.aluracursos.screenmatch.model.DatosTemporadas;
+import com.aluracursos.screenmatch.principal.Principal;
 import com.aluracursos.screenmatch.service.ConsumoAPI;
 import com.aluracursos.screenmatch.service.ConvierteDatos;
 import org.springframework.boot.CommandLineRunner;
@@ -21,23 +22,20 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		var consumoApi = new ConsumoAPI();
-		var json = consumoApi.obtenerDatos("https://www.omdbapi.com/?t=breaking+bad&apikey=a047acaa");
-		System.out.println(json);
-		ConvierteDatos conversor = new ConvierteDatos();
-		var datos = conversor.obtenerDatos(json, DatosSerie.class);
-		System.out.println(datos);
+		Principal principal = new Principal();
+		principal.muestraElMenu();
 
-		json = consumoApi.obtenerDatos("https://www.omdbapi.com/?t=breaking+bad&Season=1&episode=1&apikey=a047acaa");
-		DatosEpisodio episodios = conversor.obtenerDatos(json, DatosEpisodio.class);
-		System.out.println(episodios);
+		//se optimizo en la clase Principal
+//		var consumoApi = new ConsumoAPI();
+//		var json = consumoApi.obtenerDatos("https://www.omdbapi.com/?t=breaking+bad&apikey=a047acaa");
+//		System.out.println(json);
+//		ConvierteDatos conversor = new ConvierteDatos();
+//		var datos = conversor.obtenerDatos(json, DatosSerie.class);
+//		System.out.println(datos);
+//
+//		json = consumoApi.obtenerDatos("https://www.omdbapi.com/?t=breaking+bad&Season=1&episode=1&apikey=a047acaa");
+//		DatosEpisodio episodios = conversor.obtenerDatos(json, DatosEpisodio.class);
+//		System.out.println(episodios);
 
-		List<DatosTemporadas> temporadas = new ArrayList<>();
-		for (int i = 1; i <= datos.totalDeTemporadas(); i++) {
-			json = consumoApi.obtenerDatos("https://www.omdbapi.com/?t=breaking+bad&Season=" + i + "&apikey=a047acaa");
-			var datosTemporadas = conversor.obtenerDatos(json, DatosTemporadas.class);
-			temporadas.add(datosTemporadas);
-		}
-		temporadas.forEach(System.out::println);
 	}
 }
